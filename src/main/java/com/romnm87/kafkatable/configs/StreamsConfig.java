fa5e85197f0f6f3c2119bf4ea327ea81a55427f7
Roman Matveev <r.matveev@socrat.marketing>
2024-08-01 16:17:19 +1000

    01.08.2024 -- Создание и заполнение класса StreamsConfig.java

---
+++ src/main/java/com/romnm87/kafkatable/configs/StreamsConfig.java
@@ -0,0 +1,33 @@
+package com.romnm87.kafkatable.configs;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class StreamsConfig {
+    @Bean
+    public Properties kafkaStreamsProps() {
+        Properties properties = new Properties();
+        properties.put(org.apache.kafka.streams.StreamsConfig.APPLICATION_ID_CONFIG, "my-streams-app");
+        properties.put(org.apache.kafka.streams.StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        return properties;
+    }
+
+    @Bean
+    public Properties kafkaProducerProps() {
+        Properties properties = new Properties();
+        properties.put(org.apache.kafka.streams.StreamsConfig.APPLICATION_ID_CONFIG, "my-streams-app-2");
+        properties.put(org.apache.kafka.streams.StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        return properties;
+    }
+
+    @Bean
+    public KafkaProducer<String,String> kvKafkaProducer(Properties kafkaProducerProps) {
+        return new KafkaProducer<String, String>(kafkaProducerProps);
+    }
+}
