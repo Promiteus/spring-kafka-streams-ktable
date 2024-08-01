2958e785d14d654e8b6d7f6ffd5914a0effa02b6
Roman Matveev <r.matveev@socrat.marketing>
2024-08-01 16:13:31 +1000

    01.08.2024 -- Первичный коммит проекта.

---
+++ src/main/java/com/romnm87/kafkatable/KafkaTableApplication.java
@@ -0,0 +1,13 @@
+package com.romnm87.kafkatable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/romnm87/kafkatable/KafkaTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romnm87.kafkatable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
