1bcaf1c7f8532990646e95d17f86a92e1d100ddf
Roman Matveev <r.matveev@socrat.marketing>
2024-08-01 22:29:11 +1000

    01.08.2024 -- Класс GroupPurchaseTopology.java: описание метода process()

--- src/main/java/com/romnm87/kafkatable/topologies/GroupPurchaseTopology.java
+++ src/main/java/com/romnm87/kafkatable/topologies/GroupPurchaseTopology.java
@@ -1,13 +1,34 @@
 package com.romnm87.kafkatable.topologies;
 
+import com.romnm87.kafkatable.dtos.Purchase;
 import com.romnm87.kafkatable.topologies.interfaces.IGroupPurchaseTopology;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.*;
+import org.springframework.kafka.support.serializer.JsonSerde;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GroupPurchaseTopology implements IGroupPurchaseTopology {
+    private final static String PURCHASE_INPUT_TOPIC = "purchases";
+    public final static String PURCHASES_GROUPS_STORE = "purchases_groups_store";
+
     @Override
     public void process(StreamsBuilder streamsBuilder) {
+        KStream<String, Purchase> purchasesSteam = streamsBuilder.stream(PURCHASE_INPUT_TOPIC, Consumed.with(Serdes.String(), new JsonSerde<>(Purchase.class)))
+                .selectKey((k, v) -> v.getId());
 
+        purchasesSteam.print(Printed.<String, Purchase>toSysOut().withLabel("purchases_topic"));
+
+        KGroupedStream<String, Purchase> purchaseGroups = purchasesSteam.map((key, purchase) -> KeyValue.pair(purchase.getId(), purchase))
+                .groupByKey(Grouped.with(Serdes.String(), new JsonSerde<>(Purchase.class)));
+
+        KTable<String, Purchase> purchaseKTable = purchaseGroups.reduce((purchase, v1) -> Purchase.builder()
+                .currency(purchase.getCurrency())
+                .id(purchase.getId())
+                .name(purchase.getName()).price(purchase.getPrice()+v1.getPrice()).build(), Materialized.as(PURCHASES_GROUPS_STORE));
+
+        purchaseKTable.toStream().print(Printed.<String, Purchase>toSysOut().withLabel("purchases_groups"));
     }
 }
